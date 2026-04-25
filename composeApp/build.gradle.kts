6e17d5d8342e4b86e4cc9e731a25fc2de3ec7bd8
Lo_op <shaikhfarhaan312@gmail.com>
2026-04-25 15:37:53 -0400

    created shared core

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -54,7 +54,7 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            implementation(projects.shared)
+            implementation(projects.shared.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
