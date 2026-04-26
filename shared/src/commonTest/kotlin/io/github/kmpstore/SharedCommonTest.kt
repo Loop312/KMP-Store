09ce7308007749b0d42b2f0e1715c4c4e224017c
Lo_op <shaikhfarhaan312@gmail.com>
2026-04-26 01:12:42 -0400

    add simple database connection test

--- shared/src/commonTest/kotlin/io/github/kmpstore/SharedCommonTest.kt
+++ shared/src/commonTest/kotlin/io/github/kmpstore/SharedCommonTest.kt
@@ -1,5 +1,11 @@
 package io.github.kmpstore
 
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.Columns
+import io.github.kmpstore.di.authModule
+import kotlinx.coroutines.test.runTest
+import org.koin.core.context.startKoin
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -8,5 +14,19 @@ class SharedCommonTest {
     @Test
     fun example() {
         assertEquals(3, 1 + 2)
+    }
+
+    @Test
+    fun connectToDatabase() = runTest {
+        val koin = startKoin {
+            modules(authModule)
+        }.koin
+        val client: SupabaseClient = koin.get<SupabaseClient>()
+        println(client)
+
+        println("getting test_table from database")
+        val table = client.from("test_table").select(Columns.ALL).data
+        println("TABLE:\n$table")
+        println("__________________")
     }
 }
