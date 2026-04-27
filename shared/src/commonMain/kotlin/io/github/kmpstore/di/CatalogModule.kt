05e74b922ba7a2e99b31186d56cf3bf722ca05dd
Lo_op <shaikhfarhaan312@gmail.com>
2026-04-27 17:47:31 -0400

    rewrite all screen models to view models to use with navigation library and add product view model to CatalogModule.kt

--- shared/src/commonMain/kotlin/io/github/kmpstore/di/CatalogModule.kt
+++ shared/src/commonMain/kotlin/io/github/kmpstore/di/CatalogModule.kt
@@ -3,9 +3,14 @@ package io.github.kmpstore.di
 import io.github.kmpstore.data.repository.SupabaseProductRepository
 import io.github.kmpstore.domain.repository.ProductRepository
 import io.github.kmpstore.presentation.catalog.CatalogViewModel
+import io.github.kmpstore.presentation.product.ProductViewModel
 import org.koin.dsl.module
 
 val catalogModule = module {
     single<ProductRepository> { SupabaseProductRepository(get()) }
-    factory { CatalogViewModel(get()) }
+
+    factory<CatalogViewModel> { CatalogViewModel(get()) }
+    factory { (productId: String) ->
+        ProductViewModel(productId = productId, repository = get())
+    }
 }
