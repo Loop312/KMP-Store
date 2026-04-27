840028b9f568cec6a145ec6752c54f6b710a7d78
Lo_op <shaikhfarhaan312@gmail.com>
2026-04-27 13:05:45 -0400

    center loading indicator in CatalogScreen.kt

--- composeApp/src/commonMain/kotlin/io/github/kmpstore/presentation/catalog/CatalogScreen.kt
+++ composeApp/src/commonMain/kotlin/io/github/kmpstore/presentation/catalog/CatalogScreen.kt
@@ -1,5 +1,6 @@
 package io.github.kmpstore.presentation.catalog
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
@@ -16,19 +18,20 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 fun CatalogScreen(viewModel: CatalogViewModel = koinViewModel()) {
     val state by viewModel.state.collectAsState()
-
-    if (state.isLoading) {
-        CircularProgressIndicator()
-    } else {
-        if (state.error != null) {
-            Text(state.error!!)
-        }
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(vertical = 16.dp)
-        ) {
-            items(state.topics) { category ->
-                CategoryRow(category)
+    Box(Modifier.fillMaxSize()) {
+        if (state.isLoading) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        } else {
+            if (state.error != null) {
+                Text(state.error!!)
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(vertical = 16.dp)
+            ) {
+                items(state.topics) { category ->
+                    CategoryRow(category)
+                }
             }
         }
     }
