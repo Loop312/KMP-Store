28e40bc6cd55e3a9a1614385014d34d255838161
Lo_op <shaikhfarhaan312@gmail.com>
2026-04-27 18:26:19 -0400

    add nav3 for navigation

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -59,6 +59,9 @@ kotlin {
             implementation(libs.koin.compose.viewmodel)
             implementation(libs.coil.compose)
             implementation(libs.coil.network)
+            implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.jetbrains.material3.adaptiveNavigation3)
+            implementation(libs.jetbrains.lifecycle.viewmodelNavigation3)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
