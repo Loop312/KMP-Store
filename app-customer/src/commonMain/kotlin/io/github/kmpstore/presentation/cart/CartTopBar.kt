ede3b8d07b2259f1c82e9741667c9dd2cec08640
Lo_op <shaikhfarhaan312@gmail.com>
2026-07-07 19:28:33 -0400

    align theme toggle button better in cart screen

--- app-customer/src/commonMain/kotlin/io/github/kmpstore/presentation/cart/CartTopBar.kt
+++ app-customer/src/commonMain/kotlin/io/github/kmpstore/presentation/cart/CartTopBar.kt
@@ -1,12 +1,15 @@
 package io.github.kmpstore.presentation.cart
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import io.github.kmpstore.pad
 import io.github.kmpstore.theme.ThemeToggleButton
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,6 +19,7 @@ fun CartTopBar() {
         title = { Text("Cart") },
         actions = {
             ThemeToggleButton(Modifier.size(32.dp))
+            Spacer(Modifier.width(pad))
         }
     )
 }
